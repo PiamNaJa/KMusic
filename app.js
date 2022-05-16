277297f0e5589d3152e31f77bfcfc122102bccd9
“PiamNaJa“ <peumza1150@gmail.com>
2022-05-16 14:14:43 +0700

    l

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const   indexRoutes  = require('./routes/index'),
         playlistRoutes  = require('./routes/playlist'),
         adminRoutes  = require('./routes/admin');
 
-mongoose.connect('mongodb+srv://Kmusic:Kmusic@mycluster.vesir.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'); // เชื่อม Database
-// mongoose.connect('mongodb://localhost/Kmusic');
+// mongoose.connect('mongodb+srv://Kmusic:Kmusic@mycluster.vesir.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'); // เชื่อม Database
+mongoose.connect('mongodb://localhost/Kmusic');
 app.set("view engine" ,"ejs");
 app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({extended : true}));
